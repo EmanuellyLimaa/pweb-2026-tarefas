3dca61eb4ff298e3e97c1d5bfe1b623c5b7101e4
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-06-29 13:04:50 -0300

    dando continuidade

---
+++ tarefa04/tarefas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=200)
+    status = models.BooleanField()
+    prazo = models.DateField()
+
+    def __str__(self):
+        return self.nome
---
+++ tarefa04/tarefas/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Tarefa
+from datetime import date
+
+def index(request):
+
+    tarefas = Tarefa.objects.all()
+
+    context = {
+        'tarefas': tarefas,
+        'hoje': date.today()
+    }
+
+    return render(request, 'tarefas/index.html', context)
