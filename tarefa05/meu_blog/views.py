8350c700a9509368ad68ce8630bf9d69359add67
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-07-05 21:37:38 -0300

    atividade finalizada

--- tarefa05/meu_blog/views.py
+++ tarefa05/meu_blog/views.py
@@ -3,3 +3,8 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.all().order_by('-data')
+    return render(request, 'index.html', {'post': posts})
+
+def post(request, id):
+    postagem = get_object_or_404(Post, id=id)
+    return render(request, 'post.html', {'post': postagem})
