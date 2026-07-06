b671dcc4bdb0f21c68dbb268c83e55495b72fb4f
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-07-05 21:11:42 -0300

    criando html

--- tarefa05/meu_blog/models.py
+++ tarefa05/meu_blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    imagem = models.ImageField(upload_to='post/')
+    data = models.DateField()
+
+    def __str__(self):
+        return self.titulo
